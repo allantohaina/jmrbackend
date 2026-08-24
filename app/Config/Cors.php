def4d23ecf9261b2d201ce762d5dba3014530538
allan tohaina <mayacalipsofirst@gmail.com>
2026-08-24 12:32:44 +0300

    Add app.jmrtextile.com to CORS allowed origins

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -40,11 +40,13 @@ class Cors extends BaseConfig
             'https://api.jmrtextile.com',
             'https://admin.jmrtextile.com',
             'https://worker.jmrtextile.com',
+            'https://app.jmrtextile.com',
             'http://jmrtextile.com',
             'http://www.jmrtextile.com',
             'http://api.jmrtextile.com',
             'http://admin.jmrtextile.com',
             'http://worker.jmrtextile.com',
+            'http://app.jmrtextile.com',
         ],
 
         /**
