b70cbd77f461ded93fc163950598704831d678c1
allan tohaina <mayacalipsofirst@gmail.com>
2026-08-24 12:04:44 +0300

    Enable supportsCredentials in CORS config

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -68,7 +68,7 @@ class Cors extends BaseConfig
          *
          * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials
          */
-        'supportsCredentials' => false,
+        'supportsCredentials' => true,
 
         /**
          * Set headers to allow.
